eac6a270030e368032376edfc3a40f3ec1177544
bermics <bermics@dunwoody.edu>
2025-02-10 04:45:57 -0600

    Initial commit for GadgetHub

---
+++ GadgetHub.Domain/Entities/Gadget.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GadgetHub.Domain.Entities;
+
+namespace GadgetHub.Domain.Entities
+{
+    public class Gadget
+    {
+        public int GadgetId { get; set; }
+        public string Name {  get; set; }
+        public string Brand { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+
+    }
+}
---
+++ GadgetHub.WebUI/Controllers/GadgetController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GadgetHub.Domain.Abstract;
+using GadgetHub.Domain.Entities;
+
+
+namespace GadgetHub.WebUI.Controllers
+{
+    public class GadgetController : Controller
+    {
+        private readonly IGadgetRepository repository;
+
+        public GadgetController(IGadgetRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Gadgets);
+        }
+    }
+}
