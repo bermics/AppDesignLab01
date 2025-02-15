48712c2dd63b19ee80dda3afa05230ce977cbced
bermics <bermics@dunwoody.edu>
2025-02-14 23:25:07 -0600

    Updated project with latest changes

--- GadgetHub.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ GadgetHub.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -8,6 +8,7 @@ using GadgetHub.Domain.Abstract;
 using GadgetHub.Domain.Entities;
 using Ninject.Planning.Targets;
 using System.Web.Mvc;
+using GadgetHub.Domain.Concrete;
 
 namespace GadgetHub.WebUI.Infrastructure
 {
@@ -33,17 +34,18 @@ namespace GadgetHub.WebUI.Infrastructure
 
         private void AddBinding()
         {
+            mykernel.Bind<IGadgetRepository>().To<EFGadgetRepository>();
             // Mock Repository
-            var mock = new Mock<IGadgetRepository>();
-            mock.Setup(m => m.Gadgets).Returns(new List<Gadget>
-            {
-                new Gadget { GadgetId = 1, Name = "Galaxy S23", Brand = "Samsung", Price = 500, Description = "Samsung Smartphone with Galaxy AI", Category = "Smartphones" },
-                new Gadget { GadgetId = 2, Name = "ASUS TUF Gaming Laptop A15", Brand = "ASUS", Price = 699, Description = "144 Hz Gaming Laptop", Category = "Laptops" },
-                new Gadget { GadgetId = 3, Name = "Samsung Buds Pro 3", Brand = "Samsung", Price = 149, Description = "3rd Gen of Samsung Earbuds", Category = "Headphones" },
-                new Gadget { GadgetId = 4, Name = "Focusrite Scarlett Solo 3rd Gen USB Audio Interface", Brand = "Focusrite", Price = 130, Description = "Audio Interface for Guitarists, Vocalists, Podcasters, etc.", Category = "Accessories" }
-            });
+            //var mock = new Mock<IGadgetRepository>();
+            //mock.Setup(m => m.Gadgets).Returns(new List<Gadget>
+            //{
+            //    new Gadget { GadgetId = 1, Name = "Galaxy S23", Brand = "Samsung", Price = 500, Description = "Samsung Smartphone with Galaxy AI", Category = "Smartphones" },
+            //    new Gadget { GadgetId = 2, Name = "ASUS TUF Gaming Laptop A15", Brand = "ASUS", Price = 699, Description = "144 Hz Gaming Laptop", Category = "Laptops" },
+            //    new Gadget { GadgetId = 3, Name = "Samsung Buds Pro 3", Brand = "Samsung", Price = 149, Description = "3rd Gen of Samsung Earbuds", Category = "Headphones" },
+            //    new Gadget { GadgetId = 4, Name = "Focusrite Scarlett Solo 3rd Gen USB Audio Interface", Brand = "Focusrite", Price = 130, Description = "Audio Interface for Guitarists, Vocalists, Podcasters, etc.", Category = "Accessories" }
+            //});
 
-            mykernel.Bind<IGadgetRepository>().ToConstant(mock.Object);
+            //mykernel.Bind<IGadgetRepository>().ToConstant(mock.Object);
         }
     }
 }
